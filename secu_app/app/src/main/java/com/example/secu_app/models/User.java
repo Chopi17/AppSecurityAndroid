c854191d653279ed3f0acb4843f6624a1736ddd4
Nesel <matthieu0608@hotmail.fr>
2021-02-10 18:44:49 +0100

    Create model (User)

--- secu_app/app/src/main/java/com/example/secu_app/models/User.java
+++ secu_app/app/src/main/java/com/example/secu_app/models/User.java
@@ -1,14 +1,23 @@
 package com.example.secu_app.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
 
     private int id;
     private String name;
     private String lastname;
-    private String utilisateur;
-    private int password;
+    private List<Account> accounts = new ArrayList<>();
+    private String password;
 
-    public User() {
+    //CONSTRUCTOR
+    public User(int id, String name, String lastname, List<Account> accounts, String password) {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+        this.accounts = accounts;
+        this.password = password;
     }
 
     //GETTERS
@@ -25,14 +34,41 @@ public class User {
         return lastname;
     }
 
-    public String getUtilisateur() {
-        return utilisateur;
+    public List<Account> getAccounts() {
+        return accounts;
     }
 
     //SETTERS
 
-    public void setPassword(int password) {
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String UserToString(){
+        return "User{" +
+                "id" + id +
+                "name" + name +
+                "lastname" + lastname +
+                "password" + password +
+                "accounts" + accounts +
+                "}";
     }
 
 }
